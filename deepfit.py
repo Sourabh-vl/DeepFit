91c037f61b86f92fa1e5a93df602879643b9fa45
Sourabh-vl <56428242+Sourabh-vl@users.noreply.github.com>
2019-10-11 14:43:24 +0530

    Create deepfit.py

---
+++ deepfit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 14:39:00 2019
+
+@author: sourabhag
+"""
+
+class Deepfit(Object):
+    def __init__(self, data):
+        self.data = data
+        
+    def recordVideo(data):
+        return self.data
+    
+    def tagVideo():
+        return self.data
+        
+    
+        
+        
