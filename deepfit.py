d45847c54b64928fe587ac7c91e7160ad6c58c48
Sourabh-vl <56428242+Sourabh-vl@users.noreply.github.com>
2019-11-13 15:22:18 +0530

    Update deepfit.py
    Updated VideoToFrames method

--- deepfit.py
+++ deepfit.py
@@ -4,11 +4,32 @@ Created on Fri Oct 11 14:39:00 2019
 
 @author: sourabhag
 """
-
+import cv2
 class Deepfit:
     
-    def recordVideo(data):
-        return data
+    def VideoToFrames(path):
+        
+            # Path to video file
+        vidObj = cv2.VideoCapture(path)
+
+        # Used as counter variable
+        count = 0
+
+        # checks whether frames were extracted
+        success = 1
+
+        while success:
+
+            # vidObj object calls read
+            # function extract frames
+            success, image = vidObj.read()
+
+            # Saves the frames with frame-count
+            cv2.imwrite("frames/frame%d.jpg" % count, image)
+
+            count += 1
+        
+        
     
     def tagVideo(data):
         return data
